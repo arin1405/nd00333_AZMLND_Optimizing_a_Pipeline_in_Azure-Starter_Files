3a3b923dccb209ee7c34f7d0724823e31dbe68c6
Arindam Banerjee <arindam.banerjee14@yahoo.co.in>
2021-02-03 08:48:36 +0000

    Added files after adding codes.

--- train.py
+++ train.py
@@ -14,13 +14,14 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
+#x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
 ### YOUR CODE HERE ###a
+#x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 run = Run.get_context()
 
@@ -62,6 +63,9 @@ def main():
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
+    
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
